8bf4f139759b5cf5c0d8c2c6b119732a56a31d3b
Re-z <re-z@ukr.net>
2019-12-24 02:12:17 +0200

    in progress

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function showSelect() {
+        const flagSelect = document.querySelector('#country-select');
+        const flagDropdown = document.querySelector('.header__flag-dropdown');
+
+        flagSelect.addEventListener('click', () => {
+            if(flagDropdown.classList.contains('is-visible')) {
+                flagDropdown.classList.remove('is-visible')
+            } else {
+                flagDropdown.classList.add('is-visible')
+            }
+        });
+    }
+    showSelect()
+
+})
